3eeaad2e2925f7969ab4ef89cebcedaf1401d231
jirenguliukang <292582592@qq.com>
2019-03-24 23:50:51 +0800

    基本实现 col 和 row 的功能

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12788,7 +12788,34 @@ render._withStripped = true
       }
     })();
 },{"./icon":"src/icon.vue","_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"src/row.vue":[function(require,module,exports) {
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+//
+//
+//
+//
+//
+var _default = {
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [String, Number]
+    }
+  },
+  mounted: function mounted() {
+    var _this = this;
+
+    this.$children.forEach(function (vm) {
+      vm.gutter = _this.gutter;
+      console.log(vm.gutter);
+    });
+  }
+};
+exports.default = _default;
         var $de05a8 = exports.default || module.exports;
       
       if (typeof $de05a8 === 'function') {
@@ -12801,7 +12828,18 @@ render._withStripped = true
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "row" }, [_vm._t("default")], 2)
+  return _c(
+    "div",
+    {
+      staticClass: "row",
+      style: {
+        marginLeft: -_vm.gutter / 2 + "px",
+        marginRight: -_vm.gutter / 2 + "px"
+      }
+    },
+    [_vm._t("default")],
+    2
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12848,12 +12886,23 @@ exports.default = void 0;
 //
 //
 //
+//
+//
+//
 var _default = {
   name: "GuluCol",
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
     }
+  },
+  data: function data() {
+    return {
+      gutter: 0
+    };
   }
 };
 exports.default = _default;
@@ -12871,9 +12920,25 @@ exports.default = _default;
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "col", class: ["col-" + _vm.span] },
-    [_vm._t("default")],
-    2
+    {
+      staticClass: "col",
+      class: [
+        _vm.span && "col-" + _vm.span,
+        _vm.offset && "offset-" + _vm.offset
+      ],
+      style: {
+        paddingLeft: _vm.gutter / 2 + "px",
+        paddingRight: _vm.gutter / 2 + "px"
+      }
+    },
+    [
+      _c(
+        "div",
+        { staticStyle: { border: "1px solid green", height: "100px" } },
+        [_vm._t("default")],
+        2
+      )
+    ]
   )
 }
 var staticRenderFns = []
@@ -24154,7 +24219,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49561" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55360" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,25 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
+<script>
+export default {
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [String, Number]
+    }
+  },
+  mounted () {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+      console.log(vm.gutter)
+    })
+  }
+}
+</script>
+
 <style scoped lang="scss">
   .row {
     display: flex;
